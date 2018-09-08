1fc7148e871d4191c468f03a5c89ff59a6aba5ca
Christoffer Petersen <ctrpetersen@outlook.com>
2018-09-08 22:30:20 +0200

    Random color get etc

--- Midwich.py
+++ Midwich.py
@@ -1,11 +1,13 @@
 import pygame
 import sys
 from random import randint
+import numpy as np
 
 white = (255,255,255)
+screen_size = (800, 800)
 
 color_weight = 1.1
-game_display = pygame.display.set_mode((800,800))
+game_display = pygame.display.set_mode((screen_size))
 game_display.fill(white)
 clock = pygame.time.Clock()
 
@@ -19,7 +21,6 @@ def random_color():
 #   123
 #   4P5
 #   678
-
 #Read nearby pixels
 def average_color_of_px(x,y):
     nearby_pixels = []
@@ -32,11 +33,36 @@ def average_color_of_px(x,y):
     c7 = game_display.get_at((x,y-1))[:3]
     c8 = game_display.get_at((x+1,y-1))[:3]
     nearby_pixels += c1,c2,c3,c4,c5,c6,c7,c8
-    return nearby_pixels
+
+    avg_red   = 0
+    avg_green = 0
+    avg_blue  = 0
+    for r in nearby_pixels:
+        avg_red   += r[0]
+        avg_green += r[1]
+        avg_blue  += r[2]
+    avg_red   = int(avg_red   / len(nearby_pixels))
+    avg_green = int(avg_green / len(nearby_pixels))
+    avg_blue  = int(avg_blue  / len(nearby_pixels))
+
+    avg_color = (avg_red, avg_blue, avg_blue)
+    return avg_color
+
+def draw_random_pixel():
+    px_is_white = True
+
+    while px_is_white is True:
+        pos = (randint(0, screen_size[0]), randint(0, screen_size[1]))
+        px_color = game.display.get_at(pos)[:3]
+        if
+
+
+
+
 
 pygame.draw.rect(game_display, random_color(), [50,50,100,100])
 
-print(average_color_of_px(50,50))
+print(average_color_of_px(70,70))
 
 game_exit = False
 while not game_exit:
